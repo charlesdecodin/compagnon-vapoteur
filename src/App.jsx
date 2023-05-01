b3ce779fa79a1cd4f7b83a2dd598e2df7ea1ec89
Charles <charles.decodin@outlook.fr>
2023-05-01 13:18:58 +0200

    create and stylise result container

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import './App.scss';
 import Input from './components/Input'
 import Results from './components/Result'
+import Base from './images/base.png'
+import Booster from './images/booster.png'
+import Flavor from './images/flav.png'
 
 function App() {
   return (
@@ -17,6 +20,26 @@ function App() {
         <Input
           label="Pourcentage d'arôme"
           backgroundColor='#6DCC6D'
+        />
+      </div>
+      <div id='results'>
+        <Results
+          img={Base}
+          value={80}
+          bcgColor="#FF82F2"
+          title="Base"
+        />
+        <Results
+          img={Booster}
+          value={80}
+          bcgColor="#EF5555"
+          title="Booster"
+        />
+        <Results
+          img={Flavor}
+          value={80}
+          bcgColor="#6DCC6D"
+          title="Arôme"
         />
       </div>
 
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
-export default function Result() {
+export default function Result({ img, value, bcgColor, title }) {
   return (
-    <div>
-      
+    <div className='container-result'>
+      <div>
+        <img src={img} alt="" />
+        <p>{title}</p>
+      </div>
+      <div >
+        <p>{value}ML</p>
+        <div style={{ backgroundColor: bcgColor }}></div>
+      </div>
     </div>
   )
 }
